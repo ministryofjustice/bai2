10e6f33a54e2ddeda98b2b654b283d3c7bd65dc1
Ian Brechin <ian.brechin@gmail.com>
2015-10-22 14:39:15 +0100

    Set defaults on model creation

--- bai2/models.py
+++ bai2/models.py
@@ -67,9 +67,40 @@ class Bai2File(Bai2SectionModel):
 class Bai2FileHeader(Bai2SingleModel):
     code = RecordCode.file_header
 
+    def __init__(
+        self,
+        rows,
+        sender_id=None,
+        receiver_id=None,
+        creation_date=None,
+        creation_time=None,
+        file_id=None,
+        physical_record_length=None,
+        block_size=None,
+        version_number=2
+    ):
+        super(Bai2FileHeader, self).__init__(
+            rows, sender_id=sender_id, receiver_id=receiver_id,
+            creation_date=creation_date, creation_time=creation_time,
+            file_id=file_id, physical_record_length=physical_record_length,
+            block_size=block_size, version_number=version_number
+        )
+
 
 class Bai2FileTrailer(Bai2SingleModel):
     code = RecordCode.file_trailer
+
+    def __init__(
+        self,
+        rows,
+        file_control_total=None,
+        number_of_groups=None,
+        number_of_records=None
+    ):
+        super(Bai2FileTrailer, self).__init__(
+            rows, file_control_total=file_control_total,
+            number_of_groups=number_of_groups, number_of_records=number_of_records
+        )
 
 
 class Group(Bai2SectionModel):
@@ -86,9 +117,40 @@ class Group(Bai2SectionModel):
 class GroupHeader(Bai2SingleModel):
     code = RecordCode.group_header
 
+    def __init__(
+        self,
+        rows,
+        ultimate_receiver_id=None,
+        originator_id=None,
+        group_status=None,
+        as_of_date=None,
+        as_of_time=None,
+        currency=None,
+        as_of_date_modifier=None
+    ):
+        super(GroupHeader, self).__init__(
+            rows, ultimate_receiver_id=ultimate_receiver_id,
+            originator_id=originator_id, group_status=group_status,
+            as_of_date=as_of_date, as_of_time=as_of_time, currency=currency,
+            as_of_date_modifier=as_of_date_modifier
+        )
+
 
 class GroupTrailer(Bai2SingleModel):
     code = RecordCode.group_trailer
+
+    def __init__(
+        self,
+        rows,
+        group_control_total=None,
+        number_of_accounts=None,
+        number_of_records=None
+    ):
+        super(GroupTrailer, self).__init__(
+            rows, group_control_total=group_control_total,
+            number_of_accounts=number_of_accounts,
+            number_of_records=number_of_records
+        )
 
 
 class Account(Bai2SectionModel):
@@ -104,10 +166,50 @@ class Account(Bai2SectionModel):
 class AccountIdentifier(Bai2SingleModel):
     code = RecordCode.account_identifier
 
+    def __init__(
+        self,
+        rows,
+        customer_account_number=None,
+        currency=None,
+        summary_items=[]
+    ):
+        super(AccountIdentifier, self).__init__(
+            rows, customer_account_number=customer_account_number,
+            currency=currency, summary_items=summary_items
+        )
+
 
 class AccountTrailer(Bai2SingleModel):
     code = RecordCode.account_trailer
 
+    def __init__(
+        self,
+        rows,
+        account_control_total=None,
+        number_of_records=None
+    ):
+        super(AccountTrailer, self).__init__(
+            rows, account_control_total=account_control_total,
+            number_of_records=number_of_records
+        )
+
 
 class TransactionDetail(Bai2SingleModel):
     code = RecordCode.transaction_detail
+
+    def __init__(
+        self,
+        rows,
+        type_code=None,
+        amount=None,
+        funds_type=None,
+        availability=[],
+        bank_reference=None,
+        customer_reference=None,
+        text=None
+    ):
+        super(TransactionDetail, self).__init__(
+            rows, type_code=type_code, amount=amount, funds_type=funds_type,
+            availability=availability, bank_reference=bank_reference,
+            customer_reference=customer_reference, text=text
+        )
