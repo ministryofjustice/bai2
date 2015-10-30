67803adef94d6532bd70ad94ba8299fd02c9c085
Ian Brechin <ian.brechin@gmail.com>
2015-10-30 10:32:31 +0000

    Bump version number to 0.2.0

--- bai2/__init__.py
+++ bai2/__init__.py
@@ -2,4 +2,4 @@
 
 __author__ = 'MoJ'
 __email__ = 'dev@digital.justice.gov.uk'
-__version__ = '0.1.0'
+__version__ = '0.2.0'
--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ test_requirements = [
 
 setup(
     name='bai2',
-    version='0.1.0',
+    version='0.2.0',
     description="BAI2 Parser",
     long_description=readme + '\n\n' + history,
     author="MoJ",
