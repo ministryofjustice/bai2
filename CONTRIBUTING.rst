3041ae710777de4032a918abb1ecc11575b32e32
Igor <igor.ushkarev@digital.justice.gov.uk>
2018-03-05 14:02:12 +0000

    Normalise packaging details

--- CONTRIBUTING.rst
+++ CONTRIBUTING.rst
@@ -55,9 +55,9 @@ If you are proposing a feature:
 Get Started!
 ------------
 
-Ready to contribute? Here's how to set up `bai2` for local development.
+Ready to contribute? Here's how to set up ``bai2`` for local development.
 
-1. Fork the `bai2` repo on GitHub.
+1. Fork the ``bai2`` repo on GitHub.
 2. Clone your fork locally::
 
     $ git clone git@github.com:your_name_here/bai2.git
@@ -74,13 +74,12 @@ Ready to contribute? Here's how to set up `bai2` for local development.
 
    Now you can make your changes locally.
 
-5. When you're done making changes, check that your changes pass flake8 and the tests, including testing other Python versions with tox::
+5. When you're done making changes, check that your changes pass the tests, including testing other Python versions with tox::
 
-    $ flake8 bai2 tests
     $ python setup.py test
     $ tox
 
-   To get flake8 and tox, just pip install them into your virtualenv.
+   To get tox, just pip install it into your virtualenv.
 
 6. Commit your changes and push your branch to GitHub::
 
@@ -99,8 +98,8 @@ Before you submit a pull request, check that it meets these guidelines:
 2. If the pull request adds functionality, the docs should be updated. Put
    your new functionality into a function with a docstring, and add the
    feature to the list in README.rst.
-3. The pull request should work for Python 2.6, 2.7, 3.3, and 3.4, and for PyPy. Check
-   https://travis-ci.org/ministryofjustice/bai2/pull_requests
+3. The pull request should work for Python 2.6, 2.7, 3.3, and 3.4, 3.5, 3.6 and for PyPy.
+   Check https://travis-ci.org/ministryofjustice/bai2/pull_requests
    and make sure that the tests pass for all supported Python versions.
 
 Tips
--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
 import importlib
 import sys
 
@@ -9,56 +8,47 @@ try:
 except ImportError:
     from distutils.core import setup
 
-
 with open('README.rst') as readme_file:
-    readme = readme_file.read()
+    readme = readme_file.read().strip()
 
 with open('HISTORY.rst') as history_file:
-    history = history_file.read().replace('.. :changelog:', '')
+    history = history_file.read().replace('.. :changelog:', '').strip()
 
-requirements = [
-]
+package_info = importlib.import_module('bai2')
 
-__version__ = importlib.import_module('bai2').__version__
-
-# We only need enum34 for Pythons before 3.4
-if sys.version_info < (3, 4):
-    requirements.append('enum34')
-
-
-test_requirements = [
-    'mock',
-]
+install_requires = []
+tests_require = ['flake8']
+if sys.version_info[0:2] < (3, 4):
+    install_requires.append('enum34')
 
 setup(
     name='bai2',
-    version=__version__,
-    description="BAI2 Parser",
-    long_description=readme + '\n\n' + history,
-    author="MoJ",
-    author_email='dev@digital.justice.gov.uk',
+    version=package_info.__version__,
+    author=package_info.__author__,
+    author_email=package_info.__email__,
     url='https://github.com/ministryofjustice/bai2',
-    packages=[
-        'bai2',
-    ],
-    package_dir={'bai2':
-                 'bai2'},
+    license='MIT',
+    description='BAI2 Parser',
+    long_description=readme + '\n\n' + history,
+    packages=['bai2'],
+    package_dir={'bai2': 'bai2'},
     include_package_data=True,
-    install_requires=requirements,
-    license="BSD",
-    zip_safe=False,
-    keywords='bai2',
+    keywords='bai2 bookkeeping cash management balance reporting',
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
+        'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
+        'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
-        "Programming Language :: Python :: 2",
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ],
+    install_requires=install_requires,
+    tests_require=tests_require,
     test_suite='tests',
-    tests_require=test_requirements
 )
