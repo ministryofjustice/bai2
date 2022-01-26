cd2e4cce080413feb7685ec9a788327b8bca9053
Igor <igor.ushkarev@digital.justice.gov.uk>
2022-01-26 17:45:47 +0000

    Decrease noise from code-style test

--- tests/test_style.py
+++ tests/test_style.py
@@ -1,9 +1,12 @@
+import logging
 import pkg_resources
 import unittest
 
 
 class CodeStyleTestCase(unittest.TestCase):
     def test_code_style(self):
+        logger = logging.getLogger('flake8')
+        logger.setLevel(logging.ERROR)
         flake8 = pkg_resources.load_entry_point('flake8', 'console_scripts', 'flake8')
         try:
             flake8([])
