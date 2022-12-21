ed57416eb1b042321ca8b0ee2f3d2650a3c77de9
Igor <igor.ushkarev@digital.justice.gov.uk>
2022-12-21 16:21:51 +0000

    Fix code style unit test to work with current version of flake8 api

--- tests/test_style.py
+++ tests/test_style.py
@@ -8,8 +8,5 @@ class CodeStyleTestCase(unittest.TestCase):
         logger = logging.getLogger('flake8')
         logger.setLevel(logging.ERROR)
         flake8 = pkg_resources.load_entry_point('flake8', 'console_scripts', 'flake8')
-        try:
-            flake8([])
-        except SystemExit as e:
-            if e.code != 0:
-                self.fail('Code style checks failed')
+        if flake8(['--jobs', '0']):
+            self.fail('Code style checks failed')
