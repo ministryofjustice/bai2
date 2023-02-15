e9fbfc4590e16405b3e44968726816f94c225095
forforeach <forforeach@gmail.com>
2023-02-15 14:04:43 +0200

    refactor(helpers): normalize account identifiers row

--- bai2/helpers.py
+++ bai2/helpers.py
@@ -1,8 +1,34 @@
+from typing import Tuple, List
+
 from .models import Record
 from .constants import RecordCode
 
 
-def _build_record(rows):
+def _build_account_identifier_record(rows: List[Tuple[RecordCode, str]]) -> Record:
+    fields_str = ''
+    for index, row in enumerate(rows):
+        field_str = row[1]
+        commas_count = field_str.count(',')
+        # first row will have account identifier and currency (the common part of the account identifier)
+        # and then a multiple of 4 commas (the rest of the account identifier, which are summary components)
+        # the rest of the rows will have only the summary components
+        summary_commas_count = commas_count - 2 if index == 0 else commas_count
+
+        if field_str:
+            if field_str[-1] == '/':
+                fields_str += field_str[:-1]
+                if summary_commas_count % 4 != 0:
+                    # if the number of commas is not a multiple of 4, then we need to add a comma
+                    # some banks emit this extra comma, some don't, so we need to normalize it
+                    fields_str += ','
+            else:
+                fields_str += field_str
+
+    fields = fields_str.split(',')
+    return Record(code=rows[0][0], fields=fields, rows=rows)
+
+
+def _build_generic_record(rows: List[Tuple[RecordCode, str]]) -> Record:
     fields_str = ''
     for row in rows:
         field_str = row[1]
@@ -15,6 +41,22 @@ def _build_record(rows):
 
     fields = fields_str[:-1].split(',')
     return Record(code=rows[0][0], fields=fields, rows=rows)
+
+
+RecordBuilderFactory = {
+    RecordCode.file_header: _build_generic_record,
+    RecordCode.group_header: _build_generic_record,
+    RecordCode.account_identifier: _build_account_identifier_record,
+    RecordCode.transaction_detail: _build_generic_record,
+    RecordCode.account_trailer: _build_generic_record,
+    RecordCode.group_trailer: _build_generic_record,
+    RecordCode.file_trailer: _build_generic_record,
+}
+
+
+def _build_record(rows: List[Tuple[RecordCode, str]]) -> Record:
+    record_code = rows[0][0]
+    return RecordBuilderFactory[record_code](rows)
 
 
 def record_generator(lines):
